102293717f5391ac84cd72043c7166c9a487a1e5
Misha <79090171508@yandex.ru>
2023-02-12 22:54:37 +0500

    Task06

---
+++ src/main/java/Task06/chars/IAction.java
@@ -0,0 +1,10 @@
+package Task06.chars;
+
+import java.util.List;
+
+public interface IAction {
+    void Step(List<Base> group);
+    String GetInfo();
+}
+
+
---
+++ src/main/java/Task06/chars/Robber.java
@@ -0,0 +1,44 @@
+package Task06.chars;
+
+import java.util.List;
+
+
+public class Robber extends Base{
+    public Robber(List<Base> gang, int x, int y, int amount, String heroName) {
+        super(8, 3, 0, new int[]{2,4}, 10, 10, 6, false, false, "Robber", heroName);
+        super.gang = gang;
+        super.position = new Vector2(x, y);
+        super.amount = amount;
+    }
+
+    @Override
+    public void Step(List<Base> group) {
+        if(this.status.equals("dead")) return;
+
+        int nearestIndex = findAim(group);
+        Base aimNpc = group.get(nearestIndex);
+        double totalDamage = Damage(aimNpc);
+
+        if((int)nearestDistance(aimNpc) <= 1)
+        {
+            aimNpc.getDamage(totalDamage);
+            checkTheDead(aimNpc);
+            return;
+        }
+        int x = getPosition().x;
+        int y = getPosition().y;
+
+        if(aimNpc.getPosition().y > getPosition().y && checkPosition(new Vector2(x, ++y)))
+            setPosition(new Vector2(x, y));
+        else if(aimNpc.getPosition().y < getPosition().y && checkPosition(new Vector2(x, --y)))
+            setPosition(new Vector2(x, y));
+        else if(aimNpc.getPosition().x < getPosition().x && checkPosition(new Vector2(--x, y)))
+            setPosition(new Vector2(x, y));
+        else if(aimNpc.getPosition().x > getPosition().x && checkPosition(new Vector2(++x, y)))
+            setPosition(new Vector2(x, y));
+
+    }
+
+
+}
+
